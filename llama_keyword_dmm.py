70a760aa7e3c8c847756459b1fab765e5be5ddb9
Márton Kardos <power.up1163@gmail.com>
2023-10-04 17:18:29 +0200

    Added figures

--- llama_keyword_dmm.py
+++ llama_keyword_dmm.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import joblib
+import topicwizard.figures as figs
 from llama_cpp import Llama
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import CountVectorizer
@@ -46,12 +47,27 @@ with open("preprocessed.txt", "w") as f:
     f.write("\n\n".join(preprocessed_corpus))
 
 
+with open("preprocessed.txt") as f:
+    preprocessed_corpus = f.read().split("\n\n")
+
 print("Fitting topic model...")
-pipe = make_pipeline(CountVectorizer(), DMM(10))
+pipe = make_pipeline(CountVectorizer(stop_words="english"), DMM(10))
 pipe.fit(preprocessed_corpus)
 
 print("Saving model.")
 out_dir = Path("topic_models")
 out_dir.mkdir(exist_ok=True)
 joblib.dump(pipe, out_dir.joinpath("beluga_dmm_10.joblib"))
+
+print("Producing figures.")
+figures_dir = Path("figures/llama_keyword_dmm")
+figures_dir.mkdir(exist_ok=True, parents=True)
+topic_map = figs.topic_map(preprocessed_corpus, pipeline=pipe)
+topic_map.write_html(figures_dir.joinpath("topic_map.html"))
+bars = figs.topic_barcharts(preprocessed_corpus, pipeline=pipe)
+bars.write_html(figures_dir.joinpath("topic_barcharts.html"))
+topic_wordclouds = figs.topic_wordclouds(preprocessed_corpus, pipeline=pipe)
+topic_wordclouds.write_html(figures_dir.joinpath("topic_wordclouds.html"))
+word_map = figs.word_map(preprocessed_corpus, pipeline=pipe)
+word_map.write_html(figures_dir.joinpath("word_map.html"))
 print("DONE")
