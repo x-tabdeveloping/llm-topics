f88b273c4ef1ed28b1d84ae76751615f95f45542
Márton Kardos <power.up1163@gmail.com>
2023-10-04 17:19:44 +0200

    Replaced dmm with NMF

--- llama_keyword_nmf.py
+++ llama_keyword_nmf.py
@@ -4,10 +4,10 @@ import joblib
 import topicwizard.figures as figs
 from llama_cpp import Llama
 from sklearn.datasets import fetch_20newsgroups
+from sklearn.decomposition import NMF
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.pipeline import make_pipeline
 from tqdm import tqdm
-from tweetopic import DMM
 
 print("Loading data")
 corpus = fetch_20newsgroups(
@@ -51,16 +51,16 @@ with open("preprocessed.txt") as f:
     preprocessed_corpus = f.read().split("\n\n")
 
 print("Fitting topic model...")
-pipe = make_pipeline(CountVectorizer(stop_words="english"), DMM(10))
+pipe = make_pipeline(CountVectorizer(stop_words="english"), NMF(10))
 pipe.fit(preprocessed_corpus)
 
 print("Saving model.")
 out_dir = Path("topic_models")
 out_dir.mkdir(exist_ok=True)
-joblib.dump(pipe, out_dir.joinpath("beluga_dmm_10.joblib"))
+joblib.dump(pipe, out_dir.joinpath("beluga_nmf_10.joblib"))
 
 print("Producing figures.")
-figures_dir = Path("figures/llama_keyword_dmm")
+figures_dir = Path("figures/llama_keyword_nmf")
 figures_dir.mkdir(exist_ok=True, parents=True)
 topic_map = figs.topic_map(preprocessed_corpus, pipeline=pipe)
 topic_map.write_html(figures_dir.joinpath("topic_map.html"))
