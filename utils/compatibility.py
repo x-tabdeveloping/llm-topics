bd510cf71c94e2f6049249c2b30ffce7c9f3fa5e
Márton Kardos <power.up1163@gmail.com>
2023-10-27 14:29:38 +0200

    Added BERTopic

--- utils/compatibility.py
+++ utils/compatibility.py
@@ -1,4 +1,5 @@
 import numpy as np
+from bertopic import BERTopic
 from octis.dataset.dataset import Dataset
 from octis.models.model import AbstractModel
 from sklearn.base import BaseEstimator, TransformerMixin
@@ -33,4 +34,27 @@ class SklearnModel(AbstractModel):
         for _, words in enumerate(top):
             topics.append(list(words))
         results["topics"] = topics
+        return results
+
+
+class BERTopicModel(AbstractModel):
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def train_model(
+        self, dataset: Dataset, hyperparams=None, top_words=10
+    ) -> dict:
+        self.model = BERTopic(top_n_words=top_words, **self.kwargs)
+        results = dict()
+        corpus = dataset.get_corpus()
+        texts = [" ".join(words) for words in corpus]
+        self.model.fit(texts)
+        results["topic-word-matrix"] = self.model.c_tf_idf_.todense()
+        topics = []
+        for topic_id in self.model.topics_:
+            topics.append([term for term, _ in self.model.get_topic(topic_id)])
+        results["topics"] = topics
+        results["topic-document-matrix"] = self.model.approximate_distribution(
+            texts
+        )
         return results
--- utils/models.py
+++ utils/models.py
@@ -1,7 +1,7 @@
 from sklearn.decomposition import NMF
 from sklearn.feature_extraction.text import CountVectorizer
 
-from utils.compatibility import SklearnModel
+from utils.compatibility import BERTopicModel, SklearnModel
 from utils.eigen import EigenModel
 from utils.keybert import KeyBertVectorizer
 
@@ -13,3 +13,4 @@ models["NMF"] = lambda n_topics: SklearnModel(
     CountVectorizer(stop_words="english", max_features=8000), NMF(n_topics)
 )
 models["Eigen"] = lambda n_topics: EigenModel(n_topics)
+models["BERTopic"] = lambda n_topics: BERTopicModel(nr_topics=n_topics)
