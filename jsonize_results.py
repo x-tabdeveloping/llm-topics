c712f3faf9def8daf72f29ba3e1cd9f7600feae1
Márton Kardos <power.up1163@gmail.com>
2023-11-01 11:57:40 +0100

    Fixed JSONification

--- jsonize_results.py
+++ jsonize_results.py
@@ -4,9 +4,10 @@ import pickle
 with open("results.pkl", "rb") as in_file:
     results = pickle.load(in_file)
 
+records = []
 for run in results:
-    run["model_output"].pop("topic-word-matrix")
-    run["model_output"].pop("topic-document-matrix")
+    run["model_output"] = {"topics": run["model_output"]["topics"]}
+    records.append(run)
 
 with open("results.json", "w") as out_file:
-    json.dump(results, out_file)
+    json.dump(records, out_file)
