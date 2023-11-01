fcc3868edbbe6fda53df485e009da40aaef662f2
Márton Kardos <power.up1163@gmail.com>
2023-11-01 14:25:07 +0100

    Removed significance as a metric

--- utils/metrics.py
+++ utils/metrics.py
@@ -6,6 +6,5 @@ from octis.evaluation_metrics.topic_significance_metrics import KL_uniform
 metrics = {
     "Diversity": lambda dataset: TopicDiversity(),
     "Coherence": lambda dataset: Coherence(texts=dataset.get_corpus()),
-    "KL Uniform Significance": lambda dataset: KL_uniform(),
     "Centroid Word Embedding Coherence": lambda dataset: WECoherenceCentroid(),
 }
